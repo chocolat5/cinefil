0945bbf576eb33b52463044b1395f8424dfa4afa
Yurie.F <50815029+chocolat5@users.noreply.github.com>
2025-10-25 12:41:42 +0300

    feat: add bluesky (#3)

--- src/components/ui/Icons.tsx
+++ src/components/ui/Icons.tsx
@@ -273,3 +273,24 @@ export function SiteLogo(props: SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function Bluesky(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="1em"
+      height="1em"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M12 10Q2-2 2 6t5 8q-5 3-1 6t6-3q2 6 6 3t-1-6q5 0 5-8t-10 4"
+      ></path>
+    </svg>
+  );
+}
